e32d0c6355711e7f19881cb733546044013507d1
Unmesh Gundecha <upgundecha@gmail.com>
2016-10-14 00:03:16 +0530

    first commit

---
+++ src/test/java/com/example/RunCukesTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by upgundecha on 13/10/16.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCukesTest {
+}
